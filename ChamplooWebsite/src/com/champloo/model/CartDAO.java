a27b32596db59aa09e9a65c0e7c9385de69f2999
David <David@172.19.191.45>
2020-01-16 15:58:52 +0100

    implementazione preliminare Cart

--- ChamplooWebsite/src/com/champloo/model/CartDAO.java
+++ ChamplooWebsite/src/com/champloo/model/CartDAO.java
@@ -1,5 +1,65 @@
 package com.champloo.model;
 
+import java.util.HashMap;
+
+import com.champloo.bean.CartBean;
+import com.champloo.bean.ProductBean;
+import com.champloo.bean.ProductDetailsBean;
+
 public class CartDAO implements CartModel
 {
+
+	@Override
+	public boolean insertProduct(ProductBean product, ProductDetailsBean productDetails) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean modifyQuantity(int quantity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int retrieveNumberOfProducts() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public HashMap<ProductBean, ProductDetailsBean> retrieveProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public float retrieveTotal() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean deleteProduct(ProductBean productBean, ProductDetailsBean productDetails) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean clearCart(CartBean cart) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean storeCart(CartBean cart) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public CartBean retrieveCart(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- ChamplooWebsite/src/com/champloo/model/CartModel.java
+++ ChamplooWebsite/src/com/champloo/model/CartModel.java
@@ -1,6 +1,29 @@
 package com.champloo.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.champloo.bean.CartBean;
+import com.champloo.bean.ProductBean;
+import com.champloo.bean.ProductDetailsBean;
+
 public interface CartModel {
 	
+	public boolean insertProduct(ProductBean product, ProductDetailsBean productDetails);
 	
+	public boolean modifyQuantity(int quantity);
+	
+	public int retrieveNumberOfProducts();
+	
+	public HashMap<ProductBean, ProductDetailsBean> retrieveProducts();
+	
+	public float retrieveTotal();
+	
+	public boolean deleteProduct(ProductBean productBean, ProductDetailsBean productDetails);
+	
+	public boolean clearCart(CartBean cart);
+	
+	public boolean storeCart(CartBean cart);
+	
+	public CartBean retrieveCart(String username);
 }
